9aa8decfb75f80f0d1895679e71237f92f935aab
Asia M <asiam@reddies-MacBook-Pro.local>
2019-07-30 18:21:54 -0400

    added js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,17 +1,40 @@
 $(function () {
-    populateButtons(searchArray, 'searchButton', `#buttonsArea`);
+    populateButtons(searchArray, 'searchButton', '#buttonsArea');
     console.log("Page is Loaded");
 })
 
-var searchArray = [`Dog`, `Cat`, `Bird`];
+var searchArray = ['Dog', 'Cat', 'Bird'];
 
 function populateButtons(searchArray, classToAdd, areaToAddTo) {
     $(areaToAddTo).empty();
     for (var i = 0; i < searchArray.length; i++) {
-        var a = $(`<button>`);
+        var a = $('<button>');
         a.addClass(classToAdd);
-        a.attr(`data-type`, searchArray[i]);
+        a.attr('data-type', searchArray[i]);
         a.text(searchArray[i]);
         $(areaToAddTo).append(a);
     }
 }
+
+$(document).on('click', 'searchButton', function () {
+    var type = $(this).data('type');
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=Xg1HXO8hZPWBeO1qxZp5fsNYpgz77s1z&q=animal&limit=15&offset=0&rating=G&lang=en'
+    $.ajax([URL = queryURL, method = 'GET'])
+        .done(function (response) {
+            for (var i = 0; i < response.data.length; i++) {
+                var searchDiv = $('<div class = "search-item">');
+                var rating = response.data[i].rating;
+                var p = $('<p>').text('Rating= ' + rating);
+                var animated = response.data[i].images.fixed_height.url;
+                var still = response.data[i].images.fixed_height_still.url;
+                var image = $('<img>');
+                image.attr('src'.still);
+                image.attr('data-still', still);
+                image.attr('data-still', still);
+                image.attr
+
+
+
+            }
+        })
+})
